bd46101fac87bd3f967292a07a27697dfa93a173
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-06-12 19:23:57 +0200

    prism_prefix

--- scripts/observe_log.sh
+++ scripts/observe_log.sh
@@ -2,8 +2,11 @@
 # Logfile der Prism-Instanz bestimmen
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 CONFIG_FILE="$SCRIPT_DIR/../config.json"
+PRISM_PREFIX=$(jq -r '.minecraft.prismPrefixOverride // "~/.local/share/PrismLauncher"' "$CONFIG_FILE")
+# ~ im Pfad ersetzen
+PRISM_PREFIX="${PRISM_PREFIX/#\~/$HOME}"
 PRISM_INSTANCE_ID=$(jq -r '.minecraft.prismInstanceId' "$CONFIG_FILE")
-LOGFILE="$HOME/.local/share/PrismLauncher/instances/$PRISM_INSTANCE_ID/.minecraft/logs/latest.log"
+LOGFILE="$PRISM_PREFIX/instances/$PRISM_INSTANCE_ID/.minecraft/logs/latest.log"
 
 tail -F "$LOGFILE" | while read -r line; do
   if [[ "$line" == *"StateOutput State: wall"* ]]; then
