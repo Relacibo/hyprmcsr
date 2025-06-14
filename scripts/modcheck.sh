f404997a248b2898b6bf96e081ae48f5705283ec
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-06-14 13:46:35 +0200

    remove modcheck

--- scripts/modcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-JAR_PATH="$SCRIPT_DIR/../jars"
-FILE_PATH=$(find "$JAR_PATH" -type f -name "modcheck-*.jar" | head -n1)
-
-"$SCRIPT_DIR/runjar.sh" "$FILE_PATH" &
