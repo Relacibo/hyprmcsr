a4bfd8116ca938bd983774bf48ada7ba84d4b23b
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-06-14 20:04:48 +0200

    stateoutput waiting

--- scripts/observe_log.sh
+++ scripts/observe_log.sh
@@ -9,7 +9,8 @@ tail -F "$LOGFILE" | while read -r line; do
   if [[ "$line" == *"StateOutput State: wall"* ]]; then
     "$SCRIPT_DIR/toggle_mode.sh" normal
     "$SCRIPT_DIR/toggle_binds.sh" 0
-  elif [[ "$line" == *"StateOutput State: inworld,unpaused"* ]]; then
+  elif [[ "$line" == *"StateOutput State: waiting"* ]]; then
+    "$SCRIPT_DIR/toggle_mode.sh" normal
     "$SCRIPT_DIR/toggle_binds.sh" 1
   fi
 done
