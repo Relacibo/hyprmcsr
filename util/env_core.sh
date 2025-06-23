90f8b1920362e583fbc5cb80fb9f8ff5e8c9bae4
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-06-23 19:51:12 +0200

    fix use always same state dir

--- util/env_core.sh
+++ util/env_core.sh
@@ -7,7 +7,13 @@ HYPRMCSR_PROFILE="${HYPRMCSR_PROFILE:-default}"
 PROFILE_CONFIG_FILE="$CONFIG_ROOT/${HYPRMCSR_PROFILE}.profile.json"
 CONFIG_FILE="$CONFIG_ROOT/config.json"
 
-STATE_DIR="/tmp/hyprmcsr-$USER/$HYPRMCSR_PROFILE"
+# STATE_DIR aus profile config lesen, falls vorhanden (jq und Profil-Datei werden vorausgesetzt)
+STATE_DIR=$(jq -r '.stateDir // empty' "$PROFILE_CONFIG_FILE")
+
+if [ -z "$STATE_DIR" ]; then
+    STATE_DIR="/tmp/hyprmcsr-$USER/$HYPRMCSR_PROFILE"
+fi
+
 mkdir -p "$STATE_DIR"
 
 HYPRMCSR=$(realpath "$SCRIPT_DIR/../bin/hyprmcsr")
